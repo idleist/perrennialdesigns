96104f7209ae1689180ac0c92e4b8b357f7e6831
benrugman <benrugman@yahoo.co.uk>
2019-05-23 17:53:00 +0100

    changed font sizes and margins to more uniform values

--- controllers/projects_controller.js
+++ controllers/projects_controller.js
@@ -1,5 +1,8 @@
 var Project = require("../models/projects");
+var path = require("path");
+var fs = require("fs");
 
+// Create controller - creates new project document
 exports.project_create = (req, res) => {
   var projectName = req.body.name;
   var description = req.body.description;
@@ -22,13 +25,24 @@ exports.project_create = (req, res) => {
   res.redirect("/admin");
 };
 
+// Delete controller - deleted the project document and removes associated image from uploads folder
 exports.project_delete = (req, res) => {
   var id = req.params.projectid;
-  Project.findByIdAndDelete(id, function(err, id) {
+  Project.findByIdAndDelete(id, function(err, project) {
     if (err) {
       console.log(err);
     }
-    console.log(id + "deleted");
+
+    console.log(project + "deleted");
+
+    let imagePath = path.join("public/uploads", project.imageSource);
+    fs.unlink(imagePath, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`${project.imageSource} deleted`);
+    });
     res.redirect("/admin");
   });
 };
